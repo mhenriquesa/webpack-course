2acc72013937ddb04023ca750bc81ea9f1959828
Henrique <henriqueator@gmail.com>
2019-12-05 02:11:20 -0300

    Configurado para transformar sass em css, css em commonjs e carregar no dom

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path')
 
 module.exports = {
+    mode: "development",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
     },
+    module: {
+        rules: [
+          {
+            test: /\.scss$/i,
+            use: [
+                'style-loader', // Terceiro - Inject style into DOM
+                'css-loader', // Segundo - CSS -> commonjs
+                'sass-loader' // Primeiro - Sass -> CSS
+            ],
+          },
+        ],
+      },
 }
